421c26fc9269d2c89ffb1699ecd9f964facfba70
vkuzmuk <vladkuzmukalpha@gmail.com>
2021-11-01 18:27:24 +0200

    update

---
+++ app/src/main/java/com/raywenderlich/billboard/model/InfoItem.kt
@@ -0,0 +1,7 @@
+package com.raywenderlich.billboard.model
+
+data class InfoItem(
+    val viewsCounter: String? = null,
+    val emailsCounter: String? = null,
+    val callsCounter: String? = null
+)
